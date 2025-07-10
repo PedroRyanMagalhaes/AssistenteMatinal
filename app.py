042588a80b9c57d831cc80c4f0ecaad571a74c82
Pringles <pedroworkpartner@outlook.com>
2025-07-09 22:54:03 -0300

    tirando

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 import wheather
-
-
-
-
-
-
-
-
-
+import news
 
 
 
@@ -19,4 +11,13 @@ if __name__ == "__main__":
     previsao = wheather.buscar_previsao_tempo()
     print(previsao)  # Exibe a previsão do tempo no console
 
-    
+    #2 - MAPS
+
+    #3 - Buscar notícias
+    print("----- Buscando news -----")
+    noticias_tec = news.buscar_noticias("technology", 2)
+    print(noticias_tec)  # Exibe as notícias de tecnologia no console
+    noticias_ent = news.buscar_noticias("entertainment", 2)
+    print(noticias_ent)  # Exibe as notícias de entretenimento no console
+
+
